30c75f605fe4aeb88a7add7cf25fc3d445c99a8b
AkikoC <hatsuneakiko@komekokomi.id.vn>
2026-03-03 23:17:03 +0700

    api fixed?

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  async redirects() {
-    return [
-      {
-        source: "/:path((?!hml(?:/|$)|login(?:/|$)|list(?:/|$)|api(?:/|$)|_next(?:/|$)|.*\\..*).*)",
-        destination: "/hml",
-        permanent: false,
-      },
-    ];
-  },
+
 };
 
 export default nextConfig;
